7986d5b58439abb7266c1a3c7e1af4dc446981fc
Maximus <you@example.com>
2022-11-08 14:51:34 +0300

    my commit test

---
+++ others/array.js
@@ -0,0 +1,22 @@
+var array_counter = function(array) {
+    return 'В массиве находиться ' + array.length + " элементов!"; 
+};
+
+
+
+
+var multiply = function(x, y) {
+    return `${x} умножить на ${y} равно ${x * y}`;
+}
+
+
+var some_value = 452;
+
+
+
+
+module.exports = {
+    array_counter: array_counter,
+    multiply: multiply,
+    some_value: some_value
+};
